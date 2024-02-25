44da9b4534f68e6f54e03605c444ebd9a8caa290
Aizek <129169524+AItEKS@users.noreply.github.com>
2024-02-26 00:57:29 +0800

    Update main.py

--- gradient/main.py
+++ gradient/main.py
@@ -6,16 +6,19 @@ def lerp(v0, v1, t):
     return (1 - t) * v0 + t * v1
 
 
-color1 = np.array([255, 100, 0])
-color2 = np.array([0, 100, 255])
 size = 100
 image = np.zeros((size, size, 3), dtype="uint8")
+assert image.shape[0] == image.shape[1]
+color1 = np.array([255, 128, 0])
+color2 = np.array([0, 128, 255])
 
-x = np.linspace(0, 1, size)
-y = np.linspace(0, 1, size)
-xg, yg = np.meshgrid(x, y)
-colors = lerp(color2, color1, ((xg + yg) / 2)[..., np.newaxis])
-image[:, :, :] = colors.astype(np.uint8)
+for i in range(size):
+    for j in range(size):
+        t = (i + j) / (size * 2 * np.sqrt(2))
+        r = lerp(color1[0], color2[0], t)
+        g = lerp(color1[1], color2[1], t)
+        b = lerp(color1[2], color2[2], t)
+        image[i, j, :] = [r, g, b]
 
 plt.figure(1)
 plt.imshow(image)
