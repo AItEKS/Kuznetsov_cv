ef38fcd69a1ca01a96af206db3ece8b13a6f2d62
AItEKS <egorka.ku06@mail.ru>
2024-03-23 14:32:12 +0800

    Add

--- figures/main.py
+++ figures/main.py
@@ -9,35 +9,35 @@ def neighbours8(y, x):
 
 image = np.load("ps.npy.txt")
 
-type_1 = np.array([[1, 1, 1, 1, 1, 1],
+structure_1 = np.array([[1, 1, 1, 1, 1, 1],
                    [1, 1, 1, 1, 1, 1],
                    [1, 1, 1, 1, 1, 1],
                    [1, 1, 1, 1, 1, 1],
                    [0, 0, 0, 0, 0, 0],
                    [0, 0, 0, 0, 0, 0]])
 
-type_2 = np.array([[1, 1, 1, 1, 0, 0],
+structure_2 = np.array([[1, 1, 1, 1, 0, 0],
                    [1, 1, 1, 1, 0, 0],
                    [0, 0, 1, 1, 0, 0],
                    [0, 0, 1, 1, 0, 0],
                    [1, 1, 1, 1, 0, 0],
                    [1, 1, 1, 1, 0, 0]])
 
-type_3 = np.array([[1, 1, 0, 0, 1, 1],
+structure_3 = np.array([[1, 1, 0, 0, 1, 1],
                    [1, 1, 0, 0, 1, 1],
                    [1, 1, 1, 1, 1, 1],
                    [1, 1, 1, 1, 1, 1],
                    [0, 0, 0, 0, 0, 0],
                    [0, 0, 0, 0, 0, 0]])
 
-type_4 = np.array([[1, 1, 1, 1, 1, 1],
+structure_4 = np.array([[1, 1, 1, 1, 1, 1],
                    [1, 1, 1, 1, 1, 1],
                    [1, 1, 0, 0, 1, 1],
                    [1, 1, 0, 0, 1, 1],
                    [0, 0, 0, 0, 0, 0],
                    [0, 0, 0, 0, 0, 0]])
 
-type_5 = np.array([[1, 1, 1, 1, 0, 0],
+structure_5 = np.array([[1, 1, 1, 1, 0, 0],
                    [1, 1, 1, 1, 0, 0],
                    [1, 1, 0, 0, 0, 0],
                    [1, 1, 0, 0, 0, 0],
@@ -49,25 +49,25 @@ labeled = label(image)[0]
 
 print(f'Количество фигур: {labeled.max()}')
 
-ans = binary_dilation(binary_erosion(image, type_1), type_1)
+ans = binary_dilation(binary_erosion(image, structure_1), structure_1)
 print(f'Тип 1: {label(ans)[0].max()}')
 
 image -= ans
 
-ans = binary_dilation(binary_erosion(image, type_2), type_2)
+ans = binary_dilation(binary_erosion(image, structure_2), structure_2)
 print(f'Тип 2: {label(ans)[0].max()}')
 
 image -= ans
 
-ans = binary_dilation(binary_erosion(image, type_3), type_3)
+ans = binary_dilation(binary_erosion(image, structure_3), structure_3)
 print(f'Тип 3: {label(ans)[0].max()}')
 
 image -= ans
 
-ans = binary_dilation(binary_erosion(image, type_4), type_4)
+ans = binary_dilation(binary_erosion(image, structure_4), structure_4)
 print(f'Тип 4: {label(ans)[0].max()}')
 
 image -= ans
 
-ans = binary_dilation(binary_erosion(image, type_5), type_5)
+ans = binary_dilation(binary_erosion(image, structure_5), structure_5)
 print(f'Тип 5: {label(ans)[0].max()}')
