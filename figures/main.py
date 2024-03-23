4be09b4dbed10b83f6abec31d6f1d06604f5599f
AItEKS <egorka.ku06@mail.ru>
2024-03-23 14:23:24 +0800

    Add

--- figures/main.py
+++ figures/main.py
@@ -1,34 +1,92 @@
 import numpy as np
-from scipy.ndimage import binary_opening, label
+from scipy.ndimage import label
+from skimage.morphology import binary_erosion, binary_dilation
 
 
-def count_objects(image, structure):
-    opened_image = binary_opening(image, structure)
-    labeled_image, num_objects = label(opened_image)
-    return num_objects
+def neighbours8(y, x):
+    return (y, x - 1), (y - 1, x), (y, x + 1), (y + 1, x), (y - 1, x - 1), (y - 1, x + 1), (y + 1, x - 1), (y + 1, x + 1)
 
 
-image = np.load('ps.npy.txt')
+def get_boundaries(LB, label=1):
+    pos = np.where(LB == label)
+    bounds = []
+    for y, x in zip(*pos):
+        for yn, xn in neighbours8(y, x):
+            if yn < 0 or xn < 0 or yn >= LB.shape[0] or xn >= LB.shape[1] or LB[yn, xn] == 0:
+                bounds.append((y, x))
+                break
+    return bounds
 
-structures = [
-    np.array([[1, 1],
-              [1, 0],
-              [1, 1]]),
 
-    np.array([[1, 1, 1],
-              [1, 1, 1]]),
+def get_shape(bounds):
+    y_min, x_min = bounds[0][0], bounds[0][1]
+    LB = np.zeros((6, 6))
+    for y, x in bounds:
+        LB[y - y_min, x - x_min] = 1
+    return LB
 
-    np.array([[1, 1, 1],
-              [1, 0, 1]])
-]
 
-total_objects = 0
-objects_count = []
+image = np.load("ps.npy.txt")
 
-for structure in structures:
-    num_objects = count_objects(image, structure)
-    total_objects += num_objects
-    objects_count.append(num_objects)
+type_1 = np.array([[1, 1, 1, 1, 1, 1],
+                  [1, 1, 1, 1, 1, 1],
+                  [1, 1, 1, 1, 1, 1],
+                  [1, 1, 1, 1, 1, 1],
+                  [0, 0, 0, 0, 0, 0],
+                  [0, 0, 0, 0, 0, 0]])
 
-print(f"Общее количество всех объектов в файле: {total_objects}")
-print(f"Количество объектов для каждого вида по отдельности: {objects_count}")
+type_2 = np.array([[1, 1, 1, 1, 0, 0],
+                  [1, 1, 1, 1, 0, 0],
+                  [0, 0, 1, 1, 0, 0],
+                  [0, 0, 1, 1, 0, 0],
+                  [1, 1, 1, 1, 0, 0],
+                  [1, 1, 1, 1, 0, 0]])
+
+type_3 = np.array([[1, 1, 0, 0, 1, 1],
+                  [1, 1, 0, 0, 1, 1],
+                  [1, 1, 1, 1, 1, 1],
+                  [1, 1, 1, 1, 1, 1],
+                  [0, 0, 0, 0, 0, 0],
+                  [0, 0, 0, 0, 0, 0]])
+
+type_4 = np.array([[1, 1, 1, 1, 1, 1],
+                  [1, 1, 1, 1, 1, 1],
+                  [1, 1, 0, 0, 1, 1],
+                  [1, 1, 0, 0, 1, 1],
+                  [0, 0, 0, 0, 0, 0],
+                  [0, 0, 0, 0, 0, 0]])
+
+type_5 = np.array([[1, 1, 1, 1, 0, 0],
+                  [1, 1, 1, 1, 0, 0],
+                  [1, 1, 0, 0, 0, 0],
+                  [1, 1, 0, 0, 0, 0],
+                  [1, 1, 1, 1, 0, 0],
+                  [1, 1, 1, 1, 0, 0]])
+
+
+labeled = label(image)[0]
+
+print(f'Количество фигур: {labeled.max()}')
+
+ans = binary_dilation(binary_erosion(image, type_1), type_1)
+print(f'Тип 1: {label(ans)[0].max()}')
+
+image -= ans
+
+ans = binary_dilation(binary_erosion(image, type_2), type_2)
+print(f'Тип 2: {label(ans)[0].max()}')
+
+image -= ans
+
+ans = binary_dilation(binary_erosion(image, type_3), type_3)
+print(f'Тип 3: {label(ans)[0].max()}')
+
+image -= ans
+
+ans = binary_dilation(binary_erosion(image, type_4), type_4)
+print(f'Тип 4: {label(ans)[0].max()}')
+
+image -= ans
+
+ans = binary_dilation(binary_erosion(image, type_5), type_5)
+print(f'Тип 5: {label(ans)[0].max()}')
---
+++ trex/main.py
@@ -0,0 +1,65 @@
+import pyautogui as gui
+import keyboard
+from PIL import Image, ImageGrab
+import time
+import math
+
+
+def get_pixel(image, x, y):
+    px = image.load()
+    width, height = image.size
+    if 0 <= x < width and 0 <= y < height:
+        return px[x, y]
+    else:
+        return None
+
+
+def start():
+    x, y, width, height = 705, 180, 1260, 350
+
+    time.sleep(5)
+
+    while True:
+        jumping_time = 0
+        last_jumping_time = 0
+        current_jumping_time = 0
+        last_interval_time = 0
+
+        t1 = time.time()
+
+        if keyboard.is_pressed('q'):
+            break
+
+        sct_img = gui.screenshot(region=(x, y, width, height))
+        sct_img.save("dino.jpg")
+
+        bg_color = get_pixel(sct_img, 100, 100)
+
+        x_start, x_end = 45, 100
+        y_search1, y_search2 = 135, 138
+        y_search_for_bird = 105
+
+        for i in reversed(range(x_start, x_end)):
+            if get_pixel(sct_img, i, y_search1) != bg_color or get_pixel(sct_img, i, y_search2) != bg_color:
+                keyboard.press('up')
+                jumping_time = time.time()
+                current_jumping_time = jumping_time
+                break
+            if get_pixel(sct_img, i, y_search_for_bird) != bg_color:
+                keyboard.press("down")
+                time.sleep(0.4)
+                keyboard.release("down")
+                break
+
+        interval_time = current_jumping_time - last_jumping_time
+
+        if last_interval_time != 0 and math.floor(interval_time) != math.floor(last_interval_time):
+            x_end += 2
+            if x_end >= width:
+                x_end = width
+
+        last_jumping_time = jumping_time
+        last_interval_time = interval_time
+
+
+start()
