7a3061e0dbdb9d6d905f98f83c1630c279c184b7
AItEKS <egorka.ku06@mail.ru>
2024-03-24 19:17:44 +0800

    Add

--- trex/main.py
+++ trex/main.py
@@ -1,6 +1,6 @@
 import pyautogui as gui
 import keyboard
-from PIL import Image, ImageGrab
+from PIL import ImageGrab
 import time
 import math
 
@@ -15,43 +15,36 @@ def get_pixel(image, x, y):
 
 
 def start():
-    x, y, width, height = 705, 180, 1260, 350
+    x, y, width, height = 710, 180, 1260, 350
+    jumping_time = 0
+    last_jumping_time = 0
+    last_interval_time = 0
 
-    time.sleep(5)
+    time.sleep(3)
 
     while True:
-        jumping_time = 0
-        last_jumping_time = 0
-        current_jumping_time = 0
-        last_interval_time = 0
-
-        t1 = time.time()
-
         if keyboard.is_pressed('q'):
             break
 
-        sct_img = gui.screenshot(region=(x, y, width, height))
-        sct_img.save("dino.jpg")
-
+        sct_img = ImageGrab.grab(bbox=(x, y, x + width, y + height))
         bg_color = get_pixel(sct_img, 100, 100)
 
-        x_start, x_end = 45, 100
+        x_start, x_end = 80, 95
         y_search1, y_search2 = 135, 138
-        y_search_for_bird = 105
+        y_search_for_bird = 103
 
         for i in reversed(range(x_start, x_end)):
             if get_pixel(sct_img, i, y_search1) != bg_color or get_pixel(sct_img, i, y_search2) != bg_color:
                 keyboard.press('up')
                 jumping_time = time.time()
-                current_jumping_time = jumping_time
                 break
             if get_pixel(sct_img, i, y_search_for_bird) != bg_color:
                 keyboard.press("down")
-                time.sleep(0.4)
+                time.sleep(0.3)
                 keyboard.release("down")
                 break
 
-        interval_time = current_jumping_time - last_jumping_time
+        interval_time = jumping_time - last_jumping_time
 
         if last_interval_time != 0 and math.floor(interval_time) != math.floor(last_interval_time):
             x_end += 2
