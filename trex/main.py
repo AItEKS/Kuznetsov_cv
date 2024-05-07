669388259f2b90dbfc9c9737e436d73b1a6a9613
AItEKS <egorka.ku06@mail.ru>
2024-05-07 16:49:22 +0800

    Add

--- trex/main.py
+++ trex/main.py
@@ -4,6 +4,8 @@ import cv2
 import numpy as np
 import time
 import math
+import matplotlib.pyplot as plt
+
 
 def get_pixel(image, x, y):
     if 0 <= x < image.shape[1] and 0 <= y < image.shape[0]:
@@ -11,15 +13,16 @@ def get_pixel(image, x, y):
     else:
         return None
 
+
 def start():
     x, y, width, height = 710, 180, 550, 350
     jumping_time = 0
     last_jumping_time = 0
     last_interval_time = 0
 
-    x_start, x_end = 80, 90
-    y_search1, y_search2 = 132, 138
-    y_search_for_bird = 102
+    x_start, x_end = 76, 85
+    y_search1, y_search2 = 115, 135
+    y_search_for_bird = 99
 
     time.sleep(3)
     keyboard.press('up')
@@ -35,20 +38,42 @@ def start():
             if np.any(screenshot[y_search1, i] != bg_color) or np.any(screenshot[y_search2, i] != bg_color):
                 keyboard.press('up')
                 jumping_time = time.time()
+                # plt.imshow(screenshot)
+                # plt.show()
+                if x_start < 85:
+                    x_start += 3
+
                 break
+
             if np.any(screenshot[y_search_for_bird, i] != bg_color):
                 keyboard.press("down")
                 time.sleep(0.5)
                 keyboard.release("down")
+                # plt.imshow(screenshot)
+                # plt.show()
+                if x_end < 95:
+                    x_end += 2
+
                 break
 
         interval_time = jumping_time - last_jumping_time
 
         if last_interval_time and math.floor(interval_time) != math.floor(last_interval_time):
-            x_end += 4
+            x_end += 2
             x_end = min(x_end, width)
 
         last_jumping_time = jumping_time
         last_interval_time = interval_time
+
+        if np.all(screenshot[90, (235, 262)] == [83, 83, 83]):
+            jumping_time = 0
+            last_jumping_time = 0
+            last_interval_time = 0
+            x_start, x_end = 76, 85
+
+            keyboard.press('up')
+            keyboard.release('up')
+
+
 
 start()
