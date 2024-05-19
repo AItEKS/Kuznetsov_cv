d0e06114d230f004d2d53c2061080d20a4a511ea
Aizek <129169524+AItEKS@users.noreply.github.com>
2024-05-19 19:57:46 +0800

    Update main.py

--- trex/main.py
+++ trex/main.py
@@ -1,28 +1,19 @@
+import matplotlib.pyplot as plt
 import pyautogui as gui
 import keyboard
 import cv2
 import numpy as np
 import time
-import math
-import matplotlib.pyplot as plt
-
-
-def get_pixel(image, x, y):
-    if 0 <= x < image.shape[1] and 0 <= y < image.shape[0]:
-        return image[y, x]
-    else:
-        return None
 
 
 def start():
     x, y, width, height = 710, 180, 550, 350
-    jumping_time = 0
-    last_jumping_time = 0
-    last_interval_time = 0
 
-    x_start, x_end = 76, 85
-    y_search1, y_search2 = 115, 135
-    y_search_for_bird = 99
+    bg_color = [247, 247, 247]
+
+    x_start, x_end = 60, 110
+    y_search1, y_search2 = 132, 138
+    y_search_for_bird = 102
 
     time.sleep(3)
     keyboard.press('up')
@@ -32,16 +23,25 @@ def start():
         screenshot = gui.screenshot(region=(x, y, width, height))
         screenshot = cv2.cvtColor(np.array(screenshot), cv2.COLOR_RGB2BGR)
 
-        bg_color = get_pixel(screenshot, 100, 100)
-
         for i in reversed(range(x_start, x_end)):
             if np.any(screenshot[y_search1, i] != bg_color) or np.any(screenshot[y_search2, i] != bg_color):
-                keyboard.press('up')
-                jumping_time = time.time()
-                # plt.imshow(screenshot)
-                # plt.show()
-                if x_start < 85:
-                    x_start += 3
+                new_screenshot = gui.screenshot(region=(735, 310, 65, 13))
+                new_screenshot = cv2.cvtColor(np.array(new_screenshot), cv2.COLOR_RGB2BGR)
+
+                while np.any(new_screenshot != bg_color):
+                    keyboard.press('up')
+
+                    new_screenshot = gui.screenshot(region=(735, 310, 65, 13))
+                    new_screenshot = cv2.cvtColor(np.array(new_screenshot), cv2.COLOR_RGB2BGR)
+
+                    # plt.imshow(new_screenshot)
+                    # plt.show()
+
+                keyboard.press('down')
+                keyboard.release('down')
+
+                keyboard.press('down')
+                keyboard.release('down')
 
                 break
 
@@ -49,30 +49,7 @@ def start():
                 keyboard.press("down")
                 time.sleep(0.5)
                 keyboard.release("down")
-                # plt.imshow(screenshot)
-                # plt.show()
-                if x_end < 95:
-                    x_end += 2
-
                 break
-
-        interval_time = jumping_time - last_jumping_time
-
-        if last_interval_time and math.floor(interval_time) != math.floor(last_interval_time):
-            x_end += 2
-            x_end = min(x_end, width)
-
-        last_jumping_time = jumping_time
-        last_interval_time = interval_time
-
-        if np.all(screenshot[90, (235, 262)] == [83, 83, 83]):
-            jumping_time = 0
-            last_jumping_time = 0
-            last_interval_time = 0
-            x_start, x_end = 76, 85
-
-            keyboard.press('up')
-            keyboard.release('up')
 
 
 start()
